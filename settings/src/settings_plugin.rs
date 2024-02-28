202bc535f1a4236f25002969a237c00ac47c02bf
Xzihnago <xzihnago@gmail.com>
2024-02-28 16:59:10 +0800

    Update settings

--- settings/src/settings_plugin.rs
+++ settings/src/settings_plugin.rs
@@ -6,7 +6,7 @@ pub struct SettingsPlugin;
 
 impl Plugin for SettingsPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(Msaa::Sample8)
+        app.insert_resource(Msaa::Off)
             .insert_resource(Settings::default());
     }
 }
