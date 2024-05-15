da34326581f04020bdb1ebab1c721e8aae83835a
Xzihnago <xzihnago@gmail.com>
2024-05-15 21:51:14 +0800

    Remove move_speed setting

--- crates/settings/src/settings/settings_control.rs
+++ crates/settings/src/settings/settings_control.rs
@@ -3,15 +3,13 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(default)]
 pub struct ControlSettings {
-    pub move_speed: f32,
-    pub rotate_speed: f32,
+    pub mouse_sensitivity: f32,
 }
 
 impl Default for ControlSettings {
     fn default() -> Self {
         Self {
-            move_speed: 400.,
-            rotate_speed: 0.2,
+            mouse_sensitivity: 0.2,
         }
     }
 }
