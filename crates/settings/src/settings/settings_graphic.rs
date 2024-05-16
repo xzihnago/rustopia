dade4f10ab1be6604b95d3fa8a6ddf5e126b0450
Xzihnago <xzihnago@gmail.com>
2024-05-16 22:26:46 +0800

    Add SSAO setting

--- crates/settings/src/settings/settings_graphic.rs
+++ crates/settings/src/settings/settings_graphic.rs
@@ -1,13 +1,16 @@
 use bevy::{
     core_pipeline::{core_3d::ScreenSpaceTransmissionQuality, fxaa::Sensitivity},
+    pbr::ScreenSpaceAmbientOcclusionQualityLevel,
     render::view::Msaa,
 };
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[derive(Clone, Default, Serialize, Deserialize)]
 #[serde(default)]
 pub struct GraphicsSettings {
     pub anti_aliasing: AntiAliasing,
+    #[serde(with = "ScreenSpaceAmbientOcclusionQualityLevelDef")]
+    pub ssao: ScreenSpaceAmbientOcclusionQualityLevel,
     pub specular_transmission: SpecularTransmission,
 }
 
@@ -58,6 +61,19 @@ pub enum MsaaDef {
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(remote = "ScreenSpaceAmbientOcclusionQualityLevel")]
+pub enum ScreenSpaceAmbientOcclusionQualityLevelDef {
+    Low,
+    Medium,
+    High,
+    Ultra,
+    Custom {
+        slice_count: u32,
+        samples_per_slice_side: u32,
+    },
+}
+
+#[derive(Serialize, Deserialize)]
 #[serde(remote = "ScreenSpaceTransmissionQuality")]
 pub enum ScreenSpaceTransmissionQualityDef {
     Low,
@@ -75,5 +91,30 @@ impl std::fmt::Debug for AntiAliasing {
             AntiAliasing::MSAA(samples) => write!(f, "MSAA({:?})", samples),
             AntiAliasing::TAA => write!(f, "TAA"),
         }
+    }
+}
+
+// TODO: waiting for bevy 0.14 update, official Debug implementation for ScreenSpaceAmbientOcclusionQualityLevel (#13387)
+impl std::fmt::Debug for GraphicsSettings {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let ssao = match &self.ssao {
+            ScreenSpaceAmbientOcclusionQualityLevel::Low => format!("Low"),
+            ScreenSpaceAmbientOcclusionQualityLevel::Medium => format!("Medium"),
+            ScreenSpaceAmbientOcclusionQualityLevel::High => format!("High"),
+            ScreenSpaceAmbientOcclusionQualityLevel::Ultra => format!("Ultra"),
+            ScreenSpaceAmbientOcclusionQualityLevel::Custom {
+                slice_count,
+                samples_per_slice_side,
+            } => format!(
+                "Custom {{ slice_count: {}, samples_per_slice_side: {} }}",
+                slice_count, samples_per_slice_side
+            ),
+        };
+
+        write!(
+            f,
+            "GraphicsSettings {{ anti_aliasing: {:?}, ssao: {}, specular_transmission: {:?} }}",
+            self.anti_aliasing, ssao, self.specular_transmission
+        )
     }
 }
