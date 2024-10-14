c2b0f3ed3827e5d653f5ca090829655323ce32c1
Xzihnago <xzihnago@gmail.com>
2024-10-14 21:18:08 +0800

    Update main lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
-mod debug_texture;
 mod scenes;
 
 use bevy::{prelude::*, window::CursorGrabMode};
-use debug_texture::DebugTexture;
+
+use rustopia_settings::SettingsPlugin;
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(rustopia_settings::SettingsPlugin)
-            .add_systems(PreUpdate, update_cursor_grab_mode);
+        app.add_plugins(SettingsPlugin);
 
+        // app.add_plugins(scenes::MazeWorldPlugin);
         app.add_plugins(scenes::DebugWorldPlugin);
-        // .add_plugins(scenes::PhysicsWorldPlugin);
-        // .add_plugins(scenes::TerrainWorldPlugin);
+        // app.add_plugins(scenes::PhysicsWorldPlugin);
+        // app.add_plugins(scenes::TerrainWorldPlugin);
+
+        app.add_systems(PreUpdate, update_cursor_grab_mode);
     }
 }
 
@@ -22,13 +24,13 @@ fn update_cursor_grab_mode(
     mouse: Res<ButtonInput<MouseButton>>,
     mut window: Query<&mut Window>,
 ) {
-    let mut window = window.single_mut();
-
-    if keyboard.just_pressed(KeyCode::Escape) {
-        window.cursor.visible = true;
-        window.cursor.grab_mode = CursorGrabMode::None;
-    } else if mouse.just_pressed(MouseButton::Left) {
-        window.cursor.visible = false;
-        window.cursor.grab_mode = CursorGrabMode::Locked;
+    if let Ok(mut window) = window.get_single_mut() {
+        if keyboard.just_pressed(KeyCode::Escape) {
+            window.cursor.visible = true;
+            window.cursor.grab_mode = CursorGrabMode::None;
+        } else if mouse.just_pressed(MouseButton::Left) {
+            window.cursor.visible = false;
+            window.cursor.grab_mode = CursorGrabMode::Locked;
+        }
     }
 }
