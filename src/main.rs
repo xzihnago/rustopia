7bbc43422001a9d23830efbdb88340c9128b5c4c
Xzihnago <xzihnago@gmail.com>
2024-11-13 00:24:05 +0800

    Update logging to file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
-use bevy::prelude::*;
+#![windows_subsystem = "windows"]
+
+use std::fs::OpenOptions;
+
+use bevy::{
+    log::{BoxedLayer, Level, LogPlugin},
+    prelude::*,
+};
 use bevy_rapier3d::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: String::from("Rustopia"),
-                position: WindowPosition::Centered(MonitorSelection::Primary),
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(LogPlugin {
+                    level: Level::DEBUG,
+                    custom_layer,
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: String::from("Rustopia"),
+                        position: WindowPosition::Centered(MonitorSelection::Primary),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(rustopia::GamePlugin)
         .run();
+}
+
+fn custom_layer(_: &mut App) -> Option<BoxedLayer> {
+    let debug_file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open("debug.log")
+        .expect("Failed to open debug.log");
+
+    Some(Box::new(
+        bevy::log::tracing_subscriber::fmt::layer()
+            .with_ansi(false)
+            .with_writer(debug_file),
+    ))
 }
